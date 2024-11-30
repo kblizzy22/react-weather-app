5c381529759fd3090ecbef9540c0ba1b3d329ecb
kblizzy22 <kbailey322@gmail.com>
2024-11-30 12:57:59 -0500

    added Weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Kelly Bailey-Alme and is
           <a href="https://github.com/kblizzy22/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
