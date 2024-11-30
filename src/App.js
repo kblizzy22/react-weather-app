fa51b060cc922dc358e06c88cb6f55fdcaab7989
kblizzy22 <kbailey322@gmail.com>
2024-11-30 12:50:40 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kelly Bailey-Alme and is
-        <a href="https://github.com/kblizzy22/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kelly Bailey-Alme and is
+          <a href="https://github.com/kblizzy22/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
+        </footer>
+      </div>
     </div>
   );
 }
