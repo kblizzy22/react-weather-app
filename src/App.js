379d1ca48e8ba568f8427fbca97e91daee391f0d
kblizzy22 <kbailey322@gmail.com>
2024-11-30 12:38:07 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kelly Bailey-Alme and is
+        <a href="https://github.com/kblizzy22/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
+      </footer>
     </div>
   );
 }
