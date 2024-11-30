9cad213f820875206437e35a3c4024b5991e951a
kblizzy22 <kbailey322@gmail.com>
2024-11-30 14:24:14 -0500

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" />
+                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary" />
+                        <input type="submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -21,9 +21,14 @@ export default function Weather() {
             </ul>
             <div className="row">
                 <div className="col-6">
-                    <img src="" alt="Partly Cloudy" />
-                    35℉
+                    <div className="clearfix">
+                        <img src="" alt="Partly Cloudy" className="float-left" />
+                    <div className="float-left">
+                        <span className="temperature">35</span>
+                        <span className="unit">℉</span>
+                    </div>
                 </div>
+            </div>
                 <div className="col-6">
                     <ul>
                         <li>Precipitation: 15%</li>
