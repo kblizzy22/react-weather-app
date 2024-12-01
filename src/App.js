57f70e9e62832880aaa67f0b4e1f9c5cbb2b5dbd
kblizzy22 <kbailey322@gmail.com>
2024-12-01 15:53:43 -0500

    added api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Atlanta" />
         <footer>
           This project was coded by Kelly Bailey-Alme and is
           <a href="https://github.com/kblizzy22/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return (
-        <div className="Weather">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.temperature.current,
+            humidity: response.temperature.humidity,
+            date: "Saturday 12:30",
+            wind: response.wind.speed,
+            description: response.condition.description,
+            iconUrl: "",
+            city: response.city
+        });
+    }
+
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
+                        </div>
+                        <div className="col-3">
+                            <input type="submit" value="Search" className="btn btn-primary w-100" />
+                        </div>
                     </div>
-                    <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary w-100" />
+                </form>
+                <h1>{weatherData.city}</h1>
+                <ul>
+                    <li>{weatherData.date}</li>
+                    <li className="text-capitalize">{weatherData.description}</li>
+                </ul>
+                <div className="row mt-3">
+                    <div className="col-6">
+                        <div className="clearfix">
+                            <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left" />
+                        <div className="float-left">
+                            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                            <span className="unit">℉</span>
+                        </div>
                     </div>
                 </div>
-            </form>
-            <h1>Atlanta</h1>
-            <ul>
-                <li>Saturday 12:30</li>
-                <li>Partly Cloudy</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                    <div className="clearfix">
-                        <img src="" alt="Partly Cloudy" className="float-left" />
-                    <div className="float-left">
-                        <span className="temperature">35</span>
-                        <span className="unit">℉</span>
+                    <div className="col-6">
+                        <ul>
+                            <li>Humidity: {weatherData.humidity}%</li>
+                            <li>Wind: {weatherData.wind} mph</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Precipitation: 15%</li>
-                        <li>Humidity: 50%</li>
-                        <li>Wind: 34 mph</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    )
+        );
+    } else {
+        const apiKey ="91o0f2f84c841e4a7c38t44e6d3be32a";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading...";
+    }
 }
